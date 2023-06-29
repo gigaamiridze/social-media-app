51ca930da21f700247040184fec5e650001ba9fc
justtfelix <justttfelix@gmail.com>
2023-06-29 16:24:15 +0400

    Added stories data

---
+++ src/data/stories.ts
@@ -0,0 +1,42 @@
+export const stories = [
+  {
+    id: 1,
+    firstName: 'Joseph',
+  },
+  {
+    id: 2,
+    firstName: 'Angel',
+  },
+  {
+    id: 3,
+    firstName: 'White',
+  },
+  {
+    id: 4,
+    firstName: 'Olivier',
+  },
+  {
+    id: 5,
+    firstName: 'Giga',
+  },
+  {
+    id: 6,
+    firstName: 'Felix',
+  },
+  {
+    id: 7,
+    firstName: 'Justin',
+  },
+  {
+    id: 8,
+    firstName: 'Travis',
+  },
+  {
+    id: 9,
+    firstName: 'Don',
+  },
+  {
+    id: 10,
+    firstName: 'Sean',
+  },
+]
