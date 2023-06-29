c48c22d5173eb84f2bb77a3e9a6727aacdaab166
justtfelix <justttfelix@gmail.com>
2023-06-29 15:04:29 +0400

    Installed font awesome icons and used it with title component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
 import { Title } from './components';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 function App(): JSX.Element {
   return (
     <SafeAreaView>
-      <Title title="Let's Explore" />
+      <ScrollView>
+        <View style={style.headerContainer}>
+          <Title title="Let's Explore" />
+          <FontAwesomeIcon icon={faEnvelope} />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   )
 }
 
 export default App;
+
+const style = StyleSheet.create({
+  headerContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingTop: 20,
+    paddingRight: 17,
+    paddingLeft: 26,
+  }
+});
