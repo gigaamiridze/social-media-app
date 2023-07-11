73ba6c9906085258c334e6b14b3bbd5b73e20501
justtfelix <justttfelix@gmail.com>
2023-07-11 19:44:46 +0400

    Added navigate feature to profile page on the press button

--- src/components/Home/Header/Header.tsx
+++ src/components/Home/Header/Header.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { ScrollView, View, Pressable, Text } from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { Routes } from '../../../constants';
 import { Title } from '../Title';
 import { style } from './style';
 
 function Header() {
+  const { navigate } = useNavigation();
+
+  const navigateToProfileScreen = () => {
+    navigate(Routes.PROFILE);
+  }
+
   return (
     <ScrollView>
       <View style={style.header}>
         <Title title="Let's Explore" />
-        <Pressable style={style.messageIcon}>
-          <FontAwesomeIcon icon={faEnvelope} color='#CACDDE' size={20} />
+        <Pressable 
+          style={style.messageIcon}
+          onPress={navigateToProfileScreen}
+        >
+          <FontAwesomeIcon 
+            icon={faEnvelope} 
+            color='#CACDDE' 
+            size={20} 
+          />
           <View style={style.messageNumberWrapper}>
             <Text style={style.messageNumber}>2</Text>
           </View>
