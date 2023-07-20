976b9c2ec5d1c5cb61e1326defc5c09414a97a19
Giga Amiridze <gigaamiridze@gmail.com>
2023-07-20 17:36:24 +0400

    Removed the navigation function by pressing the notification icon

--- src/components/Home/Header/Header.tsx
+++ src/components/Home/Header/Header.tsx
@@ -1,27 +1,16 @@
 import React from 'react';
-import { useNavigation } from '@react-navigation/native';
-import { ScrollView, View, Pressable, Text } from 'react-native';
+import { ScrollView, View, Text } from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
-import { Routes } from '../../../constants';
 import { Title } from '../Title';
 import { style } from './style';
 
 function Header() {
-  const { navigate } = useNavigation();
-
-  const navigateToProfileScreen = () => {
-    navigate(Routes.PROFILE);
-  }
-
   return (
     <ScrollView>
       <View style={style.header}>
         <Title title="Let's Explore" />
-        <Pressable 
-          style={style.messageIcon}
-          onPress={navigateToProfileScreen}
-        >
+        <View style={style.messageIcon}>
           <FontAwesomeIcon 
             icon={faEnvelope} 
             color='#CACDDE' 
@@ -30,7 +19,7 @@ function Header() {
           <View style={style.messageNumberWrapper}>
             <Text style={style.messageNumber}>2</Text>
           </View>
-        </Pressable>
+        </View>
       </View>
     </ScrollView>
   )
