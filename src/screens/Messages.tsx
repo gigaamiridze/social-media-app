670865a897c5679dbbc2bb3b1d4b1999272d3795
Giga Amiridze <gigaamiridze@gmail.com>
2023-08-10 14:30:13 +0400

    Added titles for messages screen

--- src/screens/Messages.tsx
+++ src/screens/Messages.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView, ScrollView, View, Text } from 'react-native';
+import { StyleSheet, SafeAreaView, ScrollView, View, Text } from 'react-native';
 import { SearchInput } from '../components';
 import { globalStyle } from '../styles';
 
@@ -16,9 +16,35 @@ function Messages() {
     >
       <ScrollView showsVerticalScrollIndicator={false}>
         <SearchInput />
+        <View style={style.titlesContainer}>
+          <Text style={style.messagesTitle}>Messages</Text>
+          <Text style={style.messageQtyTitle}>(7)</Text>
+        </View>
       </ScrollView>
     </SafeAreaView>
   )
 }
 
 export default Messages;
+
+const style = StyleSheet.create({
+  titlesContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    columnGap: 7,
+    paddingTop: 25,
+    paddingBottom: 15,
+  },
+  messagesTitle: {
+    fontFamily: 'Inter',
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: '#102C56',
+  },
+  messageQtyTitle: {
+    fontFamily: 'Inter',
+    fontWeight: '400',
+    fontSize: 14,
+    color: '#FD92C6',
+  },
+});
