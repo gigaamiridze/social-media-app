dd04dace49ff43178ce0e8f708d602fd2a2179a7
Giga Amiridze <gigaamiridze@gmail.com>
2023-08-11 16:21:14 +0400

    Added titles for message item component

--- src/components/Messages/MessageItem/MessageItem.tsx
+++ src/components/Messages/MessageItem/MessageItem.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { HorizontalLine } from '../../HorizontalLine';
 import { style } from './style';
 
 function MessageItem() {
   return (
     <View style={style.messageItemContainer}>
-      <View style={[style.contactAvatar, { backgroundColor: '#FA8659' }]}>
+      <View style={[style.contactAvatar, { backgroundColor: '#fa2222' }]}>
         <Text style={style.contactInitials}>LW</Text>
       </View>
-      <Text>Message</Text>
+      <View style={style.contactDetails}>
+        <View style={style.flexBlock}>
+          <Text style={style.contactName}>Lewandowski</Text>
+          <Text style={style.timeSend}>20:45</Text>
+        </View>
+        <Text style={style.messageContent}>Hello, Nice to meet you!</Text>
+        <HorizontalLine />
+      </View>
     </View>
   )
 }
--- src/components/Messages/MessageItem/style.ts
+++ src/components/Messages/MessageItem/style.ts
@@ -3,8 +3,9 @@ import { StyleSheet } from 'react-native';
 export const style = StyleSheet.create({
   messageItemContainer: {
     flexDirection: 'row',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     columnGap: 16,
+    marginBottom: 15,
   },
   contactAvatar: {
     width: 50,
@@ -18,5 +19,33 @@ export const style = StyleSheet.create({
     fontWeight: '500',
     fontSize: 16,
     color: '#FFFFFF',
+  },
+  contactDetails: {
+    width: 283,
+    paddingTop: 3,
+  },
+  flexBlock: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  contactName: {
+    fontFamily: 'Inter',
+    fontWeight: '700',
+    fontSize: 14,
+    color: '#102C56',
+  },
+  timeSend: {
+    fontFamily: 'Inter',
+    fontWeight: '400',
+    fontSize: 14,
+    color: '#8A94A4',
+  },
+  messageContent: {
+    fontFamily: 'Inter',
+    fontWeight: '400',
+    fontSize: 14,
+    color: '#8A94A4',
+    marginBottom: 15,
   },
 });
